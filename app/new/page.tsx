12fe579d58a8a75758d2db2818306a11595ff85b
Max112.87 <max112.87>
2024-11-14 12:17:36 +0700

    18

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 import '../styles/App.css';
 import CheatSheet from '../components/CheatSheet';
+import CheatSheetList from '../components/CheatSheetList';
 import { useState } from 'react';
 export default function Home() {
 	const [updateCount, setUpdateCount] = useState(0);
-	console.log('updateCount:', updateCount);
+	//console.log('updateCount:', updateCount);
 
 	const handleUpdate = () => {
 		setUpdateCount(prev => prev + 1); // Увеличиваем текущее значение на 1
@@ -12,6 +13,7 @@ export default function Home() {
 	return (
 		<div className='App'>
 			<CheatSheet onAdd={handleUpdate} />
+			<CheatSheetList updateCount={updateCount} />
 		</div>
 	);
 }
